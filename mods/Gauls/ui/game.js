7a88120f12f0a3fa9bcf8ec5610d7f320ab1be3b
Benualdo <benualdo@msn.com>
2025-10-20 00:03:35 +0200

    Gauls

---
+++ mods/Gauls/ui/game.js
@@ -0,0 +1,23 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_GALLIC_WARRIOR': { //your unit type
+        unitType: 'UNIT_WARRIOR',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_GALLIC_WARRIOR_2': { //your unit type
+        unitType: 'UNIT_SPEARMAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_GALLIC_WARRIOR_3': { //your unit type
+        unitType: 'UNIT_PHALANX',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    
+ 
+}
+
+
+
+
+
+
